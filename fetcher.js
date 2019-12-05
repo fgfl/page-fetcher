ac8292d4acd917acdf569e77f57a2b92d6df57cb
Frederick <frederick.gfl@gmail.com>
2019-12-05 04:36:47 +0000

    Separate into callback functions.
    - moved the inline functions to separte callbacks
    - Added readline module to get user input to save

--- fetcher.js
+++ fetcher.js
@@ -5,22 +5,51 @@
 
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+// prince file download and save message
+const printSaveMsg = (err) => {
+  if (!err) {
+    fs.stat(localFile, (err, stats) => {
+      const fileSizeInBytes = stats.size;
+      console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${localFile}`);
+    });
+  }
+};
+
+// prompts user to save if exists, else save
+const promptSave = (notExists, htmlBody) => {
+  // console.log("promtSave:", notExists, htmlBody);
+  if(!notExists) {
+    rl.question(`The file ${localFile} exist. Do you want to overwrite it? (Y/N)`, ans => {
+      if (ans.toUpperCase() === 'Y') {
+        fs.writeFile(localFile, htmlBody, printSaveMsg);
+      } else if (ans.toUpperCase() === 'N') {
+        rl.close();
+      } else {
+        promptSave(exists);
+      }
+    });
+  }
+
+};
+
+const saveWebsiteToFile = (err, httpResponse, htmlBody) => {
+  fs.access(localFile, fs.constants.W_OK, (err) => promptSave(err, htmlBody));
+};
+
+
+
+// START EXECUTION
 const cliArgs = process.argv.slice(2);
 
 const url = cliArgs[0];
 const localFile = cliArgs[1];
 
-console.log(cliArgs);
-
-request(url, (error, response, body) => {
-  fs.writeFile(localFile, body, (err) => {
-    if (!err) {
-      fs.stat('index.html', (err, stats) => {
-        const fileSizeInBytes = stats.size;
-        console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${localFile}`);
-      });
-    }
-    
-  });
-});
+request(url, saveWebsiteToFile);
